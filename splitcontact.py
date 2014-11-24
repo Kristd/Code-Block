10bd87f47d3aa8a8d7580497d21fbee7056be184
Kristd <kristd@live.cn>
2014-11-25 00:28:10 +0800

    Update splitcontact.py

--- splitcontact.py
+++ splitcontact.py
@@ -2,18 +2,59 @@
 #encoding: utf-8
 
 import re
+import os
 
 def test():
-    m = re.compile(r'([0-9]{3}){1}')
-    for line in open("contact.txt"):
+    m = re.compile("([0-9]{3}){1}")
+    src = open("contact.txt", "r")
+
+    if os.path.exists("data.txt"):
+        os.remove("data.txt")
+        
+    data = open("data.txt", "w+")
+    splclist = "\"splc10\": ["
+    splcaddr = ""
+
+    line = src.readline()
+#"splc10": ["107"],
+#{"splc10": "107", "addr": ["kristd@live.cn", "kristd@live.cn"]}, 
+    while line:
         line.strip()
         
         if m.match(line):
-            str1 = line.split('\t')
-            print "branch:", str1[0], ", email:", str1[1]
+            tmparry = line.split('\t')
+            splc = tmparry[0].strip()
+            addr = tmparry[1].strip()
+            line = src.readline()
+
+            if len(addr) != 0:
+                continue
+
+            if not m.match(line.strip()) && len(line.strip()) != 0:
+                splcaddr = "{\"splc10\": \"" + splc + "\", \"addr\": [\"" + addr + "\","
+            else:
+                splcaddr = "{\"splc10\": \"" + splc + "\", \"addr\": [\"" + addr + "\"]"
+                data.write(splcaddr)
+                splcaddr = ""
+            
+            if line:
+                splclist += "\"" + splc + "\", "
+            else:
+                splclist += "\"" + splc + "\"]"
+
         else:
             line.strip()
             print "not match:", line.strip()
+            line = src.readline()
 
-if __name__=='__main__':
+    data.write(splclist)
+
+    if src:
+        src.close()
+
+    if data:
+        data.close()
+
+
+if __name__ == '__main__':
     test()
