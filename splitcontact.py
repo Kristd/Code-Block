e8b27ec12684e6bcf7856a2d1ffe933f5be889f9
Kristd <kristd@live.cn>
2014-12-03 15:12:34 +0800

    Update splitcontact.py

--- splitcontact.py
+++ splitcontact.py
@@ -10,87 +10,103 @@ outputPath = "f:\\data.txt"
 
 
 def createOutputFile(outputPath):
-	if os.path.exists(outputPath):
-		os.remove(outputPath)
-		
-	return open(outputPath, "w+")
+	try:
+		if os.path.exists(outputPath):
+			os.remove(outputPath)
 
-
-def openInputFile(inputPath):
-	if os.path.exists(inputPath):
-		return open(inputPath, "r")
-	else:
+		return open(outputPath, "w+")
+	except Exception as err:
 		return None
 
 
-def setBranchList():
-	return
+def openInputFile(inputPath):
+	try:
+		if os.path.exists(inputPath):
+			return open(inputPath, "r")
+		else:
+			return None
+	except Exception as err:
+		return None
 
 
-def setSplcAddr():
-	return
-
-
-
-def classify(inputObj, outputObj):    
-	splclist = "\"splc10\": ["          #"splc10": ["107", "405"],
-	splcaddr = "{\"splc10\": "        #{"splc10": "107", "addr": ["kristd@live.cn", "kristd@live.cn"]}, 
-
+#"splc10": ["107", "405"],
+def setBranchList(inputPath):
+	splcList = ""
 	m = re.compile("([0-9]{3}){1}")
-	line = inputObj.readline()
-	
+	line = inputPath.readline()
+	index = 0
+
 	while line:
-		addrList = []
-		while not m.match(line.strip()):
-			line = inputObj.readline()
+		if m.match(line.strip()):
+			tmp = line.split('\t')
+			splc = tmp[0].strip()
 
-		addrList.append(line.strip())
-		line = inputObj.readline()
-		
-		while not m.match(line.strip()):
-			if len(line.strip()):
-				addrList.append(line)
-			line = inputObj.readline()
-		
-		for i in range(len(addrList)):
-			if line != None:    #still have next line
+			if index == 0:
+				splcList += "\"" + splc + "\""
+				index += 1
+			else:
+				splcList += ", \"" + splc + "\""
+
+		line = inputPath.readline()
+
+	return splcList
+
+
+#{"splc10": "107", "addr": ["kristd@live.cn", "kristd@live.cn"]}, 
+def setSplcAddr(inputPath, outputPath):
+	splc10 = "\"splc10\": \"[splcNo]\", \"addr\":["
+	addrList = ""
+	buff = []
+	m = re.compile("([0-9]{3}){1}")
+	line = inputPath.readline()
+
+	while not m.match(line.strip()):
+		line = inputPath.readline()
+
+	while line:
+		if m.match(line.strip()):
+			buff.append(line)
+			line = inputPath.readline()
+
+			while not m.match(line.strip()):
+				buff.append(line)
+
+			for i in range(len(buff)):
 				if i == 0:
 					tmp = line.split('\t')
 					splc = tmp[0].strip()
 					addr = tmp[1].strip()
 
-					splclist += "\"" + splc + "\","         #deal with splc list
-					
-					if len(addrList) > 1 or len(addr.strip()) != 0:
-						splcaddr += "\"" + splc + "\", \"addr\":[" + "\"" + addr + "\","
-				else:
-					if len(addr.strip()) != 0:
-						splcaddr += "\"" + addr + "\","
-				#end if
-			else:               #it is eof already
-				if i == 0:
-					tmp = line.split('\t')
-					splc = tmp[0].strip()
-					addr = tmp[1].strip()
+					splc10.replace("[splcNo]", splc)
 
-					splclist += "\"" + splc + "\"]"         #deal with splc list
-					
-					if len(addrList) > 1 or len(addr.strip()) != 0:
-						splcaddr += "\"" + splc + "\", \"addr\":[" + "\"" + addr + "\"]"
+					if addr != "":
+						addrList += "\"" + addr + "\""
 				else:
-					if len(addr.strip()) != 0:
-						splcaddr += "\"" + addr + "\"]"
-				#end if
-			#end if
-		#end for
-		outputObj.writelines(splcaddr + "\n")
-	#end while
-	outputObj.writelines(splclist)
+					if addr == "":
+						continue
+					else:
+						if addrList != "":
+							addrList += ", \"" + addr + "\""
+						else:
+							addrList += "\"" + addr + "\""
+
+			splc10 += addrList + "]\n"
+			outputPath.writelines(splc10)
+		else:
+			line = inputPath.readline()
 
 
 if __name__ == '__main__':
+	splcListHead = "\"splc10\":["
 	src =  openInputFile(inputPath)
 	data = createOutputFile(outputPath)
 	
-	if data != None and src != None:
-		classify(src, data)
+	if src != None:
+		splcList = setBranchList(src)
+
+	if data != None:
+		splcListHead += splcList + "]"
+		data.writelines(splcListHead)
+		data.writelines("\n\n")
+		setSplcAddr(src, data)
+
