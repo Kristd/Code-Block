0b684769fb40fb754e94375dd7733e0482d99b4d
Kristd <kristd@live.cn>
2015-10-27 00:18:54 +0800

    Update randpwd.cpp

--- randpwd.cpp
+++ randpwd.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
 
 
 #define PASSWD_FILE_DEC "passwdfile_dec.log"
@@ -55,7 +56,7 @@ int createPasswds(unsigned char *passwd, int *length) {
 		return -1;
 	}
 
-	for(int i = 0; i < len; i++) {
+	for(int i = 0; i < len;) {
 		getPasswdChars(&lchar, &rchar);
 		//printf("lchar:[%c]\trchar:[%c]\n", lchar, rchar);
 
@@ -74,8 +75,14 @@ int createPasswds(unsigned char *passwd, int *length) {
 		}
 
 		res = (lchar << 4) + rchar;
+
+		if(res == (unsigned char)'0x0D' || res == (unsigned char)'0x0A') {
+			continue;
+		}
+
 		*ptr = res;
 		ptr++;
+		i++;
 	}
 
 	*ptr = '\0';
@@ -156,11 +163,15 @@ int compareStr(unsigned char *decbuf, int declen, unsigned char *encbuf, int enc
 }
 
 int main() {
-	int count = 1000;
+	int count = 3;
 	unsigned char passwd[32];
 	int len = 32;
 	unsigned char encPasswd[32];
 	int enclen = 32;
+
+	unsigned char content1[32];
+	unsigned char content2[32];
+
 	FILE *fp1 = NULL;
 	FILE *fp2 = NULL;
 
@@ -186,6 +197,23 @@ int main() {
 
 		calcPasswd(passwd, len, encPasswd, &enclen);
 		savToFiles(encPasswd, enclen, fp2);
+	}
+
+	fseek(fp1, 0, SEEK_SET);
+	fseek(fp2, 0, SEEK_SET);
+
+	memset(content1, 0, 32);
+	memset(content2, 0, 32);
+
+	while(1) {
+		if(fgets((char*)content1, 32, fp1) == NULL || fgets((char*)content2, 32, fp2) == NULL) {
+			printf("Bot of the files.\n");
+			break;
+		}
+
+		if(!strcmp((char*)content1, (char*)content2)) {
+			printf("Comparing error.\n");;
+		}
 	}
 
 	if(fp2 != NULL) {
